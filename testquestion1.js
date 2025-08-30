0e3db9085d05c08489568d8a727c1431ce797515
jovanohks <jovanoh400@gmail.com>
2025-08-30 08:23:50 +0000

    Aug 30, 2025, 8:23 AM

--- testquestion1.js
+++ testquestion1.js
@@ -2,8 +2,14 @@ let ball;
 function setup(){
     new Canvas(600,400);
     ball = new Sprite();
-
+    world.gravity.y = 10
 }
 function draw(){
-    
+    if (mouse.presses('left')){
+        spawnBall();
+    }
+}
+function spawnBall(){
+    ball = new Sprite(mouse.x,mouse.y,20);
+
 }
