db96356435df80d2af1f2d50ff1268b3352b4fbb
jovanohks <jovanoh400@gmail.com>
2025-08-30 08:35:47 +0000

    Aug 30, 2025, 8:35 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let ball;
+function setup(){
 
+    new Canvas(600,400);
+    world.gravity.y = 10
+}
+function draw(){
+    background(0); //background to stop ball from leaving 'trail' on screen
+    if (mouse.presses('left')){
+        spawnBall();
+    }
+}
+function spawnBall(){
+    ball = new Sprite(mouse.x,mouse.y,20);  //make ball sprite spawn at mouse x mouse y and d=20
+
+
+}
