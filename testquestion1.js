39347d95275430f8b978714c71f3d0f51e122922
jovanohks <jovanoh400@gmail.com>
2025-08-30 08:44:15 +0000

    Aug 30, 2025, 8:44 AM

--- testquestion1.js
+++ testquestion1.js
@@ -11,7 +11,7 @@ function draw(){
     }
 }
 function spawnBall(){
-    ball = new Sprite(mouse.x,mouse.y,20);  //make ball sprite spawn at mouse x mouse y and d=20
+    ball = new Sprite(mouse.x,mouse.y,30);  //make ball sprite spawn at mouse x mouse y and d=20
 
 
 }
